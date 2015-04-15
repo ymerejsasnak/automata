0a00acbf32a5f1192af1f11806e8c47031b2c172
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-04-15 13:58:13 -0400

    changed true/false to 0/1 in rules

--- automata.js
+++ automata.js
@@ -1,7 +1,7 @@
 $(function(){
 
 
-    var rules = [false, false, false, false, false, false, false, false]
+    var rules = [0, 0, 0, 0, 0, 0, 0, 0]
 
 
     createParents();
@@ -10,7 +10,7 @@ $(function(){
     $(".result-cell").on("click", function(){
         var cell = $(this);
         var rule = cell.parent().attr("id").slice(1);
-        rules[rule] = !rules[rule]
+        rules[rule] = (rules[rule] + 1) % 2  //toggle between 0 and 1
         cell.toggleClass("on");
         cell.toggleClass("off");
 

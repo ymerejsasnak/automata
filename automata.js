26fc98f9530d76ff58b84c2a7a3eab0f9bfe6978
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-04-15 13:54:04 -0400

    can now set rules by clicking cells on left

--- automata.js
+++ automata.js
@@ -1,6 +1,31 @@
 $(function(){
 
-   
+
+    var rules = [false, false, false, false, false, false, false, false]
+
+
+    createParents();
+
+
+    $(".result-cell").on("click", function(){
+        var cell = $(this);
+        var rule = cell.parent().attr("id").slice(1);
+        rules[rule] = !rules[rule]
+        cell.toggleClass("on");
+        cell.toggleClass("off");
+
+        console.log(rules)
+    });
+
+
+
+    $(".parent-cell").on("click", function(){
+        var cell = $(this);
+
+        cell.toggleClass("on");
+        cell.toggleClass("off");
+
+    });
 
 
 
@@ -11,7 +36,12 @@ $(function(){
 
 
 
-
+    function createParents() {
+        var mainDiv = $("#main");
+        for (var i = 0; i < 100; i++) {
+            mainDiv.append($("<div class='parent-cell off'></div>"))
+        }
+    }
 
 
 

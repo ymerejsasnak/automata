b416e96b72b8edfec94df5dcc4c4857dba0420b5
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-04-16 11:08:50 -0400

    clicking parent row changes currentRow variable...ready to make it work now

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # automata
 1 dimensional cellular automata (JS)
 (just for fun, get back into front end stuff)
+
+
+
+
+yeah this is just another mess/toy mostly for myself
--- automata.js
+++ automata.js
@@ -1,30 +1,37 @@
 $(function(){
 
 
-    var rules = [0, 0, 0, 0, 0, 0, 0, 0]
-
+    var rules = [0, 0, 0, 0, 0, 0, 0, 0];
+    var currentRow = [];
+    
 
     createParents();
 
 
+
+
+
+
+
     $(".result-cell").on("click", function(){
         var cell = $(this);
-        var rule = cell.parent().attr("id").slice(1);
+
+        var rule = cell.parent().attr("id").slice(4); //get rule number from the element's id
         rules[rule] = (rules[rule] + 1) % 2  //toggle between 0 and 1
+
         cell.toggleClass("on");
         cell.toggleClass("off");
-
-        console.log(rules)
     });
-
 
 
     $(".parent-cell").on("click", function(){
         var cell = $(this);
 
+        var cellNum = cell.attr("id").slice(4); //get rule number from the element's id
+        currentRow[cellNum] = (currentRow[cellNum] + 1) % 2  //toggle between 0 and 1
+        
         cell.toggleClass("on");
         cell.toggleClass("off");
-
     });
 
 
@@ -39,10 +46,15 @@ $(function(){
     function createParents() {
         var mainDiv = $("#main");
         for (var i = 0; i < 100; i++) {
-            mainDiv.append($("<div class='parent-cell off'></div>"))
+            currentRow[i] = 0;
+            mainDiv.append($("<div class='parent-cell off' id='cell" + i + "'></div>"))
         }
     }
 
+    function createChildren() {
+
+
+    }
 
 
 

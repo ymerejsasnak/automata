5a9b83d8621913f75a02b6e7ffd9184f09ab7800
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-04-16 12:10:19 -0400

    messy but it basically works for now

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 * 1 dimensional cellular automata (JS)
 * yeah this is just another mess/toy mostly for myself
-* definite contol buttons to add: run automata, randomize, all black, all white
-* possible control buttons: percentage random (ie 25% white), pattern repeat (ie define black black white and it repeats in parent row)
+* definite contol buttons to add: run automata, randomize, all black, all white, clear/start over
+* possible control buttons: step thru, percentage random (ie 25% white), pattern repeat (ie define black black white and it repeats in parent row)
 * will need to add function to delete all the divs so it can be run again and again...
+* will need to disable parent row at appropriate times
 
--- automata.js
+++ automata.js
@@ -1,22 +1,24 @@
 $(function(){
 
 
-    var rules = [0, 0, 0, 0, 0, 0, 0, 0];
-    var currentRow = [];
+    var rules = {"111": 0,
+                 "110": 0,
+                 "101": 0,
+                 "011": 0,
+                 "100": 0,
+                 "010": 0,
+                 "001": 0,
+                 "000": 0}
+
+    var currentRow = createParents();
+
     
-
-    createParents();
-
-
-
-
-
 
 
     $(".result-cell").on("click", function(){
         var cell = $(this);
 
-        var rule = cell.parent().attr("id").slice(4); //get rule number from the element's id
+        var rule = cell.parent().attr("id").slice(4); //get rule number from end of element's id
         rules[rule] = (rules[rule] + 1) % 2  //toggle between 0 and 1
 
         cell.toggleClass("on");
@@ -27,7 +29,7 @@ $(function(){
     $(".parent-cell").on("click", function(){
         var cell = $(this);
 
-        var cellNum = cell.attr("id").slice(4); //get rule number from the element's id
+        var cellNum = parseInt(cell.attr("id").slice(4)); //get cell number from end of element's id
         currentRow[cellNum] = (currentRow[cellNum] + 1) % 2  //toggle between 0 and 1
         
         cell.toggleClass("on");
@@ -35,8 +37,11 @@ $(function(){
     });
 
 
-
-
+    $("#run").on("click", function(){
+        for (var i = 0; i < 50; i++){
+            currentRow = createChildren(currentRow);
+        }
+    });
 
 
 
@@ -45,15 +50,46 @@ $(function(){
 
     function createParents() {
         var mainDiv = $("#main");
+        var parents = []
         for (var i = 0; i < 100; i++) {
-            currentRow[i] = 0;
+            parents[i] = 0;
             mainDiv.append($("<div class='parent-cell off' id='cell" + i + "'></div>"))
         }
+        return parents;
     }
 
-    function createChildren() {
+    function createChildren(fromRow) {
+        var mainDiv = $("#main");
+        var neighborhood = "";
+        var nextRow = [];
 
+        mainDiv.append($("<div class='spacer'></div>")); //clears floats to start new row
 
+        for (var i = 0; i < 100; i++) { 
+            //(rethink this, must be a better way to do it w/ mod or something)
+            if (i === 0) {
+                neighborhood = fromRow[99].toString() + fromRow[0].toString() + fromRow[1].toString();
+            }
+            else if (i === 99) {
+                neighborhood = fromRow[98].toString() + fromRow[99].toString() + fromRow[0].toString();
+            }
+            else {
+                neighborhood = fromRow[i - 1].toString() + fromRow[i].toString() + fromRow[i + 1].toString();
+            }
+            
+            var state = rules[neighborhood];
+
+            nextRow.push(state);
+            if (state) {
+                mainDiv.append($("<div class='child-cell on' id='cell" + i + "'></div>"));
+            }
+            else {
+                mainDiv.append($("<div class='child-cell off' id='cell" + i + "'></div>"));
+            }
+
+        }
+        
+        return nextRow;
     }
 
 
